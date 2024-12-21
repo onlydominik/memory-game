79f79797da8e95bf318bcb1a5e8c68e06b038dae
onlydominik <kdominikpl@gmail.com>
2024-12-22 00:49:38 +0100

    feat: add Header component and logoStyles.css
    Signed-off-by: onlydominik <kdominikpl@gmail.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import './logoStyles.css';
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Header() {
+  const navLinkBaseStyles = 'w-min text-lg sm:text-xl text-header-text';
+
+  const getNavLinkClassName = ({ isActive }: { isActive: boolean }): string => {
+    return `${navLinkBaseStyles} ${
+      isActive
+        ? 'underline underline-offset-8 decoration-1 text-header-textAccent'
+        : ''
+    }`;
+  };
+
+  return (
+    <header
+      className="flex flex-col items-center gap-4 sm:gap-0 pb-3 sm:pb-8 sm:mt-8 mx-auto max-w-2xl bg-header-bg rounded-b-3xl sm:rounded-3xl"
+      role="navigation"
+      aria-label="Main Navigation"
+    >
+      <div className="w-3/4 max-w-xs py-2 pb-3 text-center bg-header-logoBg shadow-logoShadow rounded-b-[3rem]">
+        <Link
+          to={'/'}
+          className="block w-full text-2xl sm:text-3xl textLogoColor"
+          aria-label="Home"
+        >
+          MIND MELD
+        </Link>
+      </div>
+      <div className="flex gap-12 flex-col xs:flex-row sm:justify-between sm:px-10 sm:mt-[-1rem] sm:w-full">
+        <NavLink
+          to={'/challenges'}
+          className={getNavLinkClassName}
+          aria-label="Challenges"
+        >
+          CHALLENGES
+        </NavLink>
+        <NavLink
+          to={'/highscores'}
+          className={getNavLinkClassName}
+          aria-label="Highscores"
+        >
+          HIGHSCORES
+        </NavLink>
+      </div>
+    </header>
+  );
+}
